586ad1cb1630397677a7de076273583399d375dc
Jawne3 <johnlowendavid@yahoo.ca>
2024-10-04 02:26:25 -0600

    Fixed export in header

--- CCForum/src/index.jsx
+++ CCForum/src/index.jsx
@@ -1,4 +1,8 @@
 
-
 import React from 'react';
 import {createRoot} from 'react-dom/client';
+import App from './App';
+
+const rootElement = document.getElementById('app');
+
+createRoot(rootElement).render(<App />);
